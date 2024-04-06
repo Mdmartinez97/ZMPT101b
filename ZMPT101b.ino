d1c08cdedc2b8dc1e4dde5d8fae1e0519991623b
Marcos Martinez Borgiattino <101603101+Mdmartinez97@users.noreply.github.com>
2024-04-06 12:55:02 -0300

    Not work on ESP8266

--- ZMPT101b.ino
+++ ZMPT101b.ino
@@ -1,20 +1,21 @@
 #include "EmonLib.h"
-
-#define VOLT_CAL 350 //Valor de calibración del sensor
-EnergyMonitor emon1; //Crea una instancia de Energy Monitor
+EnergyMonitor emon1;    // Crea una instancia de Energy Monitor
+#define VOLT_CAL 350    // Valor de calibración del sensor
+#define PHASE_SHIFT 1.7 // Defasaje
+#define ZMPTpin A0      // Sensor de Tensión GPIO
 
 //******************************************** SETUP ********************************************************************
 void setup() {
  
  Serial.begin(115200);
- emon1.voltage(32, VOLT_CAL, 1.7); //Configuración de función 'voltage' (Pin de lectura, Calibración, Cambio de Fase)
+ emon1.voltage(ZMPTpin, VOLT_CAL, PHASE_SHIFT); //Configuración de función 'voltage' (Pin de lectura, Calibración, Defasaje)
 }
 //******************************************** LOOP ********************************************************************
 void loop() {
 
  emon1.calcVI(20,2000); //Función de cálculo (20 semiciclos, tiempo de espera para tomar medición) 
  
- float tension = emon1.Vrms; //Valor V RMS obtenido
+ int tension = emon1.Vrms; //Valor V RMS obtenido
 
  Serial.print(tension);
  Serial.println("V RMS");
